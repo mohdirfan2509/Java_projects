b0697798d1c56c88bb29e6362662d9f1121b3a29
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-02-19 23:29:49 +0530

    New Files

--- Practice/src/com/pw/ioi/testing.java
+++ Practice/src/com/pw/ioi/testing.java
@@ -4,54 +4,14 @@ package com.pw.ioi;
 public class testing {
 
 	public static void main(String[] args) {
-		char [][] arr = null;
 		
-	    int n=10;
-	    
-		for(int k=0;k<5;k++) {
-			
-			int i=k;
-			int j=k;
-			n--;
-			int m=k;
-
-			
-			
-//			******* TOP ROW *******
-			while(j<n) {
-//				arr[i][j]='a';
-				System.out.println("( "+i +" , "+j+" )");
-				
-				j++;
-			}
-			
-//			******* RIGHT COLUMN *******
-			while(i<n) {
-//				arr[i][j]='a';
-				System.out.println("( "+i +" , "+j+" )");
-				i++;
-			}
-			
-//			******* BOTTOM ROW *******
-			while(j>m) {
-//				arr[i][j]='a';
-				System.out.println("( "+i +" , "+j+" )");
-				j--;
-			}
-			
-//			******* LEFT COLUMN *******
-			while(i>m){
-//				arr[i][j]='a';
-				System.out.println("( "+i +" , "+j+" )");
-				i--;
-				
-			}
-			System.out.println("********ROUND "+(k+1)+" IS DONE **********");
-		}
+		int [] arr= {1,2,3,4};
+		arr[2]=4;
+//		arr[4]=5;
+		System.out.println(arr[2]);
+//		System.out.println(arr[4]);
 		
-
 		
-
 
 	}
 
