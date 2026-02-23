84f879eeec8fbb7a4edbda7f65097eca3dc3c921
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-02-23 22:45:25 +0530

    New Files

--- Practice/src/com/pw/ioi/testing.java
+++ Practice/src/com/pw/ioi/testing.java
@@ -5,11 +5,18 @@ public class testing {
 
 	public static void main(String[] args) {
 		
-		int [] arr= {1,2,3,4};
-		arr[2]=4;
-//		arr[4]=5;
-		System.out.println(arr[2]);
-//		System.out.println(arr[4]);
+		  switch(2)
+          { 
+                case 1:
+                System.out.println("One");
+                case 2:
+                System.out.println("Two");
+                case 3:
+                System.out.println("Three");
+                default:
+                System.out.println("Default");
+                break;
+          }
 		
 		
 
