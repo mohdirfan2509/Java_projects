7127194b38f78b583d9f37f14f8cccbc27a488f8
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-02-26 22:14:31 +0530

    New Files

--- Practice/src/com/pw/ioi/Main19.java
+++ Practice/src/com/pw/ioi/Main19.java
@@ -15,25 +15,29 @@ public class Main19 {
 				System.out.println("YES");
 				System.out.println(n);
 				for(int j=1;j<=n;j++) {
-					System.out.println(1+" ");
+					System.out.print("1 ");
 				}
+				System.out.println();
 			}else {
-				if(x==1) {
+				if(k==1) {
 					System.out.println("NO");
 				}else {
 					if(n%2==0) {
 						System.out.println("YES");
 						System.out.println(n/2);
 						for(int j=1;j<=n/2;j++) {
-							System.out.println(2+" ");
+							System.out.print("2 ");
 						}
+						System.out.println();
 					}else {
 						if(k>=3) {
-							
-							for(int j=1;j<=(n/2)-1;j++) {
-								System.out.println(2+" ");
+							System.out.println("YES");
+							System.out.println(((n-3)/2)+1);
+							for(int j=1;j<=(n-3)/2;j++) {
+								System.out.print(2+" ");
 							}
 							System.out.println(3);
+							System.out.println();
 						}else {
 							System.out.println("NO");
 						}
