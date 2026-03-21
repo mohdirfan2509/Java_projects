84234ec8e28df6a3768efc982a7dc1641af0f652
mohdirfan2509 <mohammadirfan25092004@gmail.com>
2026-03-21 23:37:52 +0530

    Files

---
+++ Practice/src/com/pw/ioi/Main29.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main29 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = String.valueOf(n);
+
+            int d = s.length();
+            int firstDigit = s.charAt(0) - '0';
+
+            int ans = (d - 1) * 9 + firstDigit;
+
+            System.out.println(ans);
+        }
+
+        sc.close();
+    }
+}
